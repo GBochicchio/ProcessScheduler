6fdc68345624cceb138ee112db62d4eecad81aad
Gerardo Bochicchio <github@gbochicchio.it>
2023-06-21 12:05:19 +0200

    Implementation of the FCFS algorithm

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <ctime> // Librearia per utilizzare time()
 
-const int N = 5; // Numero di processi
+const int N = 10; // Numero di processi
 const int QUANTUM = 1; // Quantum per l'algoritmo di Round Robin
 
 class Process {
@@ -17,7 +18,28 @@ public:
 std::vector<Process> processes;
 
 void first_come_first_served() {
-    std::cout << "FCFS non ancora implementato.\n";
+    std::vector<int> waiting_time(N, 0); // Initialize waiting time for all processes
+
+    // Calculate waiting time for each process
+    for (int i = 1; i < N; i++) {
+        waiting_time[i] = waiting_time[i - 1] + processes[i - 1].burst_time;
+    }
+
+    // Calculate average waiting time
+    double avg_waiting_time = 0;
+    for (int i = 0; i < N; i++) {
+        avg_waiting_time += waiting_time[i];
+    }
+    avg_waiting_time /= N;
+
+    // Display the scheduling result
+    std::cout << "FCFS Scheduling:\n";
+    std::cout << "Process\tBurst Time\tPriority\tWaiting Time\n";
+    for (int i = 0; i < N; i++) {
+        std::cout << processes[i].pid << "\t" << processes[i].burst_time << "\t\t"
+                  << processes[i].priority << "\t\t" << waiting_time[i] << "\n";
+    }
+    std::cout << "Average Waiting Time: " << avg_waiting_time << "\n";
 }
 
 void shortest_job_first() {
@@ -45,6 +67,8 @@ void completely_fair_scheduler() {
 }
 
 int main() {
+    srand(time(nullptr)); // Imposta il seme iniziale basato sull'ora di sistema
+
     // Inizializzazione dei processi
     for (int i = 0; i < N; i++) {
         processes.emplace_back(i, rand() % 10 + 1, rand() % 3 + 1);
